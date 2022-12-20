822ef7c38d345ee05179f0c68802f1df987ead81
Vishal Kharche <83890748+VishalKharche14@users.noreply.github.com>
2022-12-20 10:09:02 +0530

    Time: 86 ms (14.15%), Space: 54.6 MB (17.64%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,16 +2,16 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int[][] dp = new int[coins.length][amount+1] ;
         for(int[] arr : dp) Arrays.fill(arr,-1) ;
-        int ans = f(0,amount,coins,dp) ;
+        int ans = f(coins.length-1,amount,coins,dp) ;
         return ans == Integer.MAX_VALUE ? -1 : ans ;
     }
     private int f(int idx, int target, int[] coins, int[][] dp){
         if(target == 0) return 0 ;
-        if (idx >= coins.length || target < 0) return Integer.MAX_VALUE ;
+        if (idx < 0 || target < 0) return Integer.MAX_VALUE ;
         
         if(dp[idx][target] != -1) return dp[idx][target] ;
         
-        int notTake = f(idx+1,target,coins,dp) ;
+        int notTake = f(idx-1,target,coins,dp) ;
         int take = f(idx,target-coins[idx],coins,dp) ;
         if(take != Integer.MAX_VALUE) take++;
         
