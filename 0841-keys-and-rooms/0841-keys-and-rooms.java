569a561ce30d699d08ddf610060714a913cd339d
Vishal Kharche <83890748+VishalKharche14@users.noreply.github.com>
2022-12-20 23:59:18 +0530

    Time: 3 ms (57.13%), Space: 45.5 MB (5.45%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,6 +1,23 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        int count = 0 ;
+    
+        // return bfs(rooms) ;
+        
+        Set<Integer> visited = new HashSet<>() ;
+        visited.add(0) ;
+        
+        return dfs(0,visited,rooms) ;
+    }
+    private boolean dfs(int room, Set<Integer> visited, List<List<Integer>> keys){
+        for(int key : keys.get(room)){
+            if(!visited.contains(key)){
+                visited.add(key) ;
+                dfs(key,visited,keys) ;
+            }
+        }
+        return keys.size() == visited.size() ;
+    }
+    private boolean bfs(List<List<Integer>> rooms) {
         Set<Integer> visited = new HashSet<>() ;
         Queue<Integer> q = new LinkedList<>() ;
         q.add(0) ;
@@ -9,14 +26,13 @@ class Solution {
             int room = q.remove() ;
             if(visited.contains(room)) continue ;
             visited.add(room) ;
-            count++;
             
             for(int key : rooms.get(room)){
                 q.add(key) ;
             }
         }
         
-        return count == rooms.size() ;
+        return visited.size() == rooms.size() ;
         
     }
 }
