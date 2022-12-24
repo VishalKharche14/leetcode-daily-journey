ab06d54d2edc738186df1f3752e6a6b2dc767b37
Vishal Kharche <83890748+VishalKharche14@users.noreply.github.com>
2022-12-24 17:39:57 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (82.13%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.java
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.java
@@ -1,4 +1,11 @@
 class Solution {
+    /*
+dp[i] = dp[i-1] + dp[i-2] + 2*(dp[i-3]+......dp[0]) 
+dp[i] = dp[i-1] + dp[i-2] +  dp[i-3] + dp[i-3] + 2*(dp[i-4]+......dp[0]) 
+dp[i] = dp[i-1] + dp[i-3] + {dp[i-2] + dp[i-3] + 2*(d[i-4] + ...... dp[0])}
+dp[i] = dp[i-1] + dp[i-3] + dp[i-1] 
+dp[i] = 2*dp[i-1] + dp[i-3] 
+*/
     public int numTilings(int n) {
         int mod = (int)(1e9+7);
         long[] dp = new long[n+1];
@@ -7,17 +14,7 @@ class Solution {
         if(n>=2) dp[2] = 2;
         
         for(int i=3; i<=n ; i++){
-            //Vertical domino
-            dp[i] += dp[i-1];
-            //Horizontal dominos
-            dp[i] += dp[i-2];
-            
-            //Tromino
-            long ans = 0;
-            for(int j=3; i-j>=0; j++){
-                ans += dp[i-j];
-            }
-            dp[i] += 2*ans ;
+            dp[i] = 2*dp[i-1] + dp[i-3] ;
             dp[i] %= mod ;
         }
         
