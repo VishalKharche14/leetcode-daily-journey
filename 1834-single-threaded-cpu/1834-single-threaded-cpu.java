6ba4ea5c19ee1ef6a0e99425f4acd7fe6ce22291
Vishal Kharche <83890748+VishalKharche14@users.noreply.github.com>
2022-12-29 17:04:09 +0530

    Time: 190 ms (73.04%), Space: 99.8 MB (72.12%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.java
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.java
@@ -19,40 +19,40 @@ class Solution {
                 return a[1] - b[1] ;
             }
         });
-        for(int i=0; i<tasks.length; i++){
-            if(minHeap.isEmpty() && endTime < tasks[i][0]){
-                endTime = tasks[i][0] ;
-            }
-            
-            if(endTime >= tasks[i][0]){
-                minHeap.add(tasks[i]) ;
-            }
-            else{
-                int[] taskToPerform = minHeap.remove();
-                sequence.add(indices.get(taskToPerform));
-                endTime += taskToPerform[1];
-                i--;
-            }
-        }
-        while(!minHeap.isEmpty()){
-            int[] taskToPerform = minHeap.remove();
-            sequence.add(indices.get(taskToPerform));
-            endTime += taskToPerform[1];    
-        }
-//         int idx = 0 ;
-//         while(idx < tasks.length || !minHeap.isEmpty()){
-//             if(minHeap.isEmpty() && endTime < tasks[idx][0]){
-//                 endTime = tasks[idx][0] ;
-//             }
-//             while(idx < tasks.length && endTime >= tasks[idx][0]){
-//                 minHeap.add(tasks[idx]) ;
-//                 ++idx ;
+//         for(int i=0; i<tasks.length; i++){
+//             if(minHeap.isEmpty() && endTime < tasks[i][0]){
+//                 endTime = tasks[i][0] ;
 //             }
             
+//             if(endTime >= tasks[i][0]){
+//                 minHeap.add(tasks[i]) ;
+//             }
+//             else{
+//                 int[] taskToPerform = minHeap.remove();
+//                 sequence.add(indices.get(taskToPerform));
+//                 endTime += taskToPerform[1];
+//                 i--;
+//             }
+//         }
+//         while(!minHeap.isEmpty()){
 //             int[] taskToPerform = minHeap.remove();
 //             sequence.add(indices.get(taskToPerform));
-//             endTime += taskToPerform[1];         
+//             endTime += taskToPerform[1];    
 //         }
+        int idx = 0 ;
+        while(idx < tasks.length || !minHeap.isEmpty()){
+            if(minHeap.isEmpty() && endTime < tasks[idx][0]){
+                endTime = tasks[idx][0] ;
+            }
+            while(idx < tasks.length && endTime >= tasks[idx][0]){
+                minHeap.add(tasks[idx]) ;
+                ++idx ;
+            }
+            
+            int[] taskToPerform = minHeap.remove();
+            sequence.add(indices.get(taskToPerform));
+            endTime += taskToPerform[1];         
+        }
 
         return sequence.stream().mapToInt(i->i).toArray() ;
     }
