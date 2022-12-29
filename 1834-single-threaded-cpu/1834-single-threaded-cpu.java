a851932fe967655d1639de6e15df5ebcc82b91af
Vishal Kharche <83890748+VishalKharche14@users.noreply.github.com>
2022-12-29 17:07:28 +0530

    Time: 186 ms (73.50%), Space: 100.1 MB (71.66%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.java
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.java
@@ -13,12 +13,19 @@ class Solution {
 
         int endTime = 0 ;
         List<Integer> sequence = new ArrayList<>() ;
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>(new Comparator<int[]>(){
-            public int compare(int[] a, int[] b){
-                if(a[1] == b[1]) return indices.get(a) - indices.get(b) ;
-                return a[1] - b[1] ;
-            }
+        
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b)->{
+            if(a[1] == b[1]) return indices.get(a) - indices.get(b) ;
+            return a[1] - b[1] ;
         });
+        
+//         PriorityQueue<int[]> minHeap = new PriorityQueue<>(new Comparator<int[]>(){
+//             public int compare(int[] a, int[] b){
+//                 if(a[1] == b[1]) return indices.get(a) - indices.get(b) ;
+//                 return a[1] - b[1] ;
+//             }
+//         });
+        
 //         for(int i=0; i<tasks.length; i++){
 //             if(minHeap.isEmpty() && endTime < tasks[i][0]){
 //                 endTime = tasks[i][0] ;
@@ -39,6 +46,7 @@ class Solution {
 //             sequence.add(indices.get(taskToPerform));
 //             endTime += taskToPerform[1];    
 //         }
+        
         int idx = 0 ;
         while(idx < tasks.length || !minHeap.isEmpty()){
             if(minHeap.isEmpty() && endTime < tasks[idx][0]){
