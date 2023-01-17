18090c8195dc02d4d28d6b3ced8aa967c911df7e
Vishal Kharche <83890748+VishalKharche14@users.noreply.github.com>
2023-01-17 16:44:27 +0530

    Time: 9 ms (92.20%), Space: 42.8 MB (84.41%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
@@ -1,4 +1,9 @@
 class Solution {
+    
+    public int minFlipsMonoIncr(String s) {
+        // return dynamicWindow(s) ;
+        return dp(s) ;
+    }
     /*
     dividing String into 2 parts -> left and right.
     left part should be All ZEROS and right part should be All ONES
@@ -11,7 +16,7 @@ class Solution {
         Similarly if we encounter a ONE -> means we need to flip it as left part should be all             ZEROS.
     
     */
-    public int minFlipsMonoIncr(String s) {
+    private int dynamicWindow(String s) {
         //counting all Zeros
         int zero = 0 ;
         for(char ch : s.toCharArray()){
@@ -25,6 +30,28 @@ class Solution {
                 ans = Math.min(ans,--zero) ;
             }
             else zero++ ;
+        }
+        
+        return ans ;
+    }
+    /*
+    dp[i] = no. of flips required to make a subString from(0 to i) monotone increasing .
+    dp[0] = 0 ; // single digit string will always be monotone increasing.
+    if s.charAt(i) == '1' then dp[i] = dp[i-1] because we can append 1 to any string to make it monotone increasing.
+    if s.charAt(i) =='0' -> it have 2 cases -> min of both will be the answer.
+        1. flip it -> if we decided to slip it will become same condition as above because we can               append 1 to any string to make it monotone increasing just our flip count will be                 increased by 1 .
+              so dp[i] = 1+dp[i-1] ;
+              
+        2. do not flip it. -> then we need to flip all the 1 present before i.
+                dp[i] = number of 1's present in substring[0,i-1] (both inclusive) .
+    */
+    private int dp(String s){
+        int one = 0 ;
+        int ans = 0 ;
+        
+        for(char ch : s.toCharArray()){
+            if(ch == '1') one++ ;
+            else ans = Math.min(1+ans,one) ;
         }
         
         return ans ;
