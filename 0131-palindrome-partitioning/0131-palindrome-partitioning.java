19a99c4182d181a7f9e0e6f63a93d23a4ecb462b
Vishal Kharche <83890748+VishalKharche14@users.noreply.github.com>
2023-01-22 12:02:37 +0530

    Time: 9 ms (90.92%), Space: 54.3 MB (94.60%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -4,10 +4,11 @@ class Solution {
     public List<List<String>> partition(String s) {
         dp = new boolean[s.length()][s.length()] ;
         list = new ArrayList<>();
-        f(0,s,new ArrayList<String>());
+        dynamicP(0,s,new ArrayList<String>());
+        // backtrack(0,s,new ArrayList<String>());
         return list ;
     }
-    private void f(int idx, String s, List<String> curr){
+    private void dynamicP(int idx, String s, List<String> curr){
         
         if(idx == s.length()){
             list.add(new ArrayList(curr));
@@ -18,9 +19,30 @@ class Solution {
             if(s.charAt(idx) == s.charAt(i) && ( i-idx <= 2 || dp[idx+1][i-1])){
                 dp[idx][i] = true ;
                 curr.add(s.substring(idx,i+1));
-                f(i+1,s,curr);
+                dynamicP(i+1,s,curr);
                 curr.remove(curr.size()-1);
             }
         }
+    }
+    private void backtrack(int idx, String s, List<String> curr){
+        
+        if(idx == s.length()){
+            list.add(new ArrayList(curr));
+            return ;
+        }
+        
+        for(int i=idx; i<s.length(); i++){
+            if(isPalindrome(s,idx,i)){
+                curr.add(s.substring(idx,i+1));
+                backtrack(i+1,s,curr);
+                curr.remove(curr.size()-1);
+            }
+        }
+    }
+    private boolean isPalindrome(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start++) != s.charAt(end--)) return false;
+        }
+        return true;
     }
 }
