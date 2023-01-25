4d754f9f5cbb33ce8281baf420d3c5cafdb84046
Vishal Kharche <83890748+VishalKharche14@users.noreply.github.com>
2023-01-25 13:06:49 +0530

    Time: 23 ms (68.26%), Space: 105.8 MB (30.54%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
@@ -2,14 +2,14 @@ class Solution {
     public int closestMeetingNode(int[] edges, int node1, int node2) {
         int[]dist1 = new int[edges.length] ;
         int[]dist2 = new int[edges.length] ;
-        Arrays.fill(dist1,edges.length+1) ;
-        Arrays.fill(dist2,edges.length+1) ;
+        Arrays.fill(dist1,edges.length) ;
+        Arrays.fill(dist2,edges.length) ;
         dist1[node1] = 0 ;
         dist2[node2] = 0 ;
         int dist = 1 ;
         
         while(edges[node1] != -1){
-            if(dist1[edges[node1]] != edges.length+1) break ;
+            if(dist1[edges[node1]] != edges.length) break ;
             dist1[edges[node1]] = dist ;
             node1 = edges[node1] ;
             dist++ ;
@@ -17,13 +17,13 @@ class Solution {
         
         dist = 1 ;
         while(edges[node2] != -1){
-            if(dist2[edges[node2]] != edges.length+1) break ;
+            if(dist2[edges[node2]] != edges.length) break ;
             dist2[edges[node2]] = dist ;
             node2 = edges[node2] ;
             dist++ ;
         }
         
-        int max = Integer.MAX_VALUE ;   
+        int max = edges.length ;   
         int ans = -1 ;
         for(int i=0; i<edges.length; i++){
             int temp = Math.max(dist1[i],dist2[i]);
@@ -33,6 +33,6 @@ class Solution {
             }
         }
         
-        return max == edges.length+1 ? - 1 : ans ;
+        return ans ;
     }
 }
