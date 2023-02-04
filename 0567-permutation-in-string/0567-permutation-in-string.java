50b60850a58a12a699597a973b88e4acebced3b6
Vishal Kharche <83890748+VishalKharche14@users.noreply.github.com>
2023-02-04 06:33:03 +0530

    Time: 5 ms (80.89%), Space: 42.2 MB (72.87%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,22 +1,37 @@
-class Solution {
+public class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        Set<Character> set = s1.chars().mapToObj(i -> (char) i).collect(Collectors.toSet());
-        int num = bitShift(s1);
-        
-        for(int i=0; i+s1.length()<=s2.length(); i++){
-            if(set.contains(s2.charAt(i))){
-                if(num == bitShift(s2.substring(i,i+s1.length()))) return true;
+        if (s1.length() > s2.length())
+            return false;
+        int[] s1map = new int[26];
+        int[] s2map = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            s1map[s1.charAt(i) - 'a']++;
+            s2map[s2.charAt(i) - 'a']++;
+        }
+
+        int count = 0;
+        for (int i = 0; i < 26; i++) {
+            if (s1map[i] == s2map[i])
+                count++;
+        }
+
+        for (int i = 0; i < s2.length() - s1.length(); i++) {
+            int r = s2.charAt(i + s1.length()) - 'a', l = s2.charAt(i) - 'a';
+            if (count == 26)
+                return true;
+            s2map[r]++;
+            if (s2map[r] == s1map[r]) {
+                count++;
+            } else if (s2map[r] == s1map[r] + 1) {
+                count--;
+            }
+            s2map[l]--;
+            if (s2map[l] == s1map[l]) {
+                count++;
+            } else if (s2map[l] == s1map[l] - 1) {
+                count--;
             }
         }
-        return false;
-    }
-
-    private int bitShift(String s1) {
-        int num = 0;
-        for(char ch : s1.toCharArray()){
-            int idx = ch - 'a' ;
-            num += (1<<idx);
-        }
-        return num;
+        return count == 26;
     }
 }
