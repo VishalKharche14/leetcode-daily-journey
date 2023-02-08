9397e009d0e811a7fea8e5cefa669d31cc8ed093
Vishal Kharche <83890748+VishalKharche14@users.noreply.github.com>
2023-02-09 02:40:19 +0530

    Time: 1 ms (99.45%), Space: 42.5 MB (83.53%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,21 +1,17 @@
 class Solution {
     public int jump(int[] nums) {
-        // The starting range of the first jump is [0, 0]
-        int answer = 0, n = nums.length;
-        int curEnd = 0, curFar = 0;
+        int currReach=0;
+        int maxReach=0;
+        int jumps=0;
         
-        for (int i = 0; i < n - 1; ++i) {
-            // Update the farthest reachable index of this jump.
-            curFar = Math.max(curFar, i + nums[i]);
-
-            // If we finish the starting range of this jump,
-            // Move on to the starting range of the next jump.
-            if (i == curEnd) {
-                answer++;
-                curEnd = curFar;
+        for(int i=0; i<nums.length-1; i++){
+                
+            maxReach=Math.max(maxReach,i+nums[i]);
+            if(i==currReach){
+                jumps++;
+                currReach=maxReach;
             }
         }
-        
-        return answer;
+        return jumps;
     }
 }
